35ebc3289b1001eeeb6296a19695101d554444ff
JV13 <cherkas.ivan13@gmail.com>
2025-07-31 10:16:53 +0300

    change logic

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Ngrx
+# angular_kinopoisk
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.2.3.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -5,12 +5,8 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -20,16 +16,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ngrx'`, () => {
+  it(`should have as title 'angular_kinopoisk'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ngrx');
+    expect(app.title).toEqual('angular_kinopoisk');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ngrx app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'angular_kinopoisk app is running!'
+    );
   });
 });
